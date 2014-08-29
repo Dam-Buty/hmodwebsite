459f802ddcbedf7838234207c5fcc3ed7346fb10
Damien BUTY <dam.buty@gmail.com>
2014-08-29 18:22:34 +0200

    Play around with Bootstrap

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,20 @@
 
 (function() {
-	var app = angular.module("store", []);
+	var app = angular.module("store", ["ngSanitize"]);
 	
 	app.controller("StoreController", [ "$http", function($http) {		
 		this.livres = [{
+		    id: "hmod_001",
 			titre: "Caucasus",
-			auteur: "Thiephaine Bidule",
+			auteur: "Tiéphaine G. Szuter",
 			prix: 1.99,
-			description: "Caucasus est un texte inquiétant signé Tiéphaine G Szuter :\n\n“Lorsqu’une expédition aussi mystérieuse que précipitée est organisée pour explorer les abimes de la Krubera-Voronja, une des grottes les plus profondes du monde, les scientifiques participants – tous russophones – se posent des questions.</p><p>Qu’y a-t-il au plus profond de ces souterrains qui ait pu piquer la curiosité du gouvernement russe ? Ce n’est que quand le chef de l’expédition, un certain Dragan Dmitrov, leur annonce qu’ils sont venus cartographier une cité ancienne enterrée là depuis des millénaires qu’ils comprennent que cette expédition pourrait bien changer leur vie à tout jamais…”</p>",
-			images: {
-				couv: "1_couv.jpg",
-				auteur: "1_auteur.jpg"
-			}
+			description: "<p>Caucasus est un texte inquiétant signé Tiéphaine G Szuter :'Lorsqu'une expédition aussi mystérieuse que précipitée est organisée pour explorer les abimes de la Krubera-Voronja, une des grottes les plus profondes du monde, les scientifiques participants à tous russophones à se posent des questions.</p><p>Qu'y a-t-il au plus profond de ces souterrains qui ait pu piquer la curiosité du gouvernement russe ? Ce n'est que quand le chef de l'expédition, un certain Dragan Dmitrov, leur annonce qu'ils sont venus cartographier une cité ancienne enterrée là depuis des millénaires qu'ils comprennent que cette expédition pourrait bien changer leur vie à tout jamais</p>"
 		}, {
+		    id: "hmod_002",
 			titre: "La maison du fond des bayous",
 			auteur: "Tanguy Mandias",
 			prix: 0.99,
-			description: "<p>Ce mois-ci, c’est deux histoires pour le prix d’une ! Dans la plus pure tradition des séances de cinéma Double Feature.</p><p><b>La maison du fond des bayous</b> : une bande de gamins téméraires décident de braver l’interdiction de leurs parents et de percer le mystère de l’étrange maison qui dort au fond des bayous. Mais tout le monde sait que la curiosité est un vilain défaut…</p><p><b>Ozymandias</b> : embrumé par l’alcool et le chagrin, on serait prêt à tout pour faire revenir la femme que l’on aime. Même à pactiser avec un étrange magicien venu des colonies.</p><p>Deux histoires d’horreur dans la plus pure tradition des magazines pulp des 50's. Un combo terrifique à souhait !</p>",
-			images: {
-				couv: "2_couv.jpg",
-				auteur: "2_auteur.jpg"
-			}
+			description: "<p>Ce mois-ci, c'est deux histoires pour le prix d'une ! Dans la plus pure tradition des séances de cinéma Double Feature.</p><p><b>La maison du fond des bayous</b> : une bande de gamins téméraires décident de braver l'interdiction de leurs parents et de percer le mystère de l'étrange maison qui dort au fond des bayous. Mais tout le monde sait que la curiosité est un vilain défaut</p><p><b>Ozymandias</b> : embrumé par l'alcool et le chagrin, on serait prêt à tout pour faire revenir la femme que l'on aime. Même à pactiser avec un étrange magicien venu des colonies.</p><p>Deux histoires d'horreur dans la plus pure tradition des magazines pulp des 50's. Un combo terrifique à souhait !</p>"
 		}];
 		
 		this.current = 0;
@@ -29,12 +23,22 @@
 			return "assets/" + this.livres[this.current].id + "_couv.jpg";
 		};
 		
+		this.getAuteur = function() {
+			return "assets/" + this.livres[this.current].id + "_auteur.jpg";
+		};
+		
 		this.getCurrent = function() {
 			return this.livres[this.current];
 		};
 		
 		this.next = function() {
-			this.current++;
+			var current = this.current + 1;
+			
+			if (this.livres[current] === undefined) {
+			    current = 0;
+			}
+			
+			this.current = current;
 		};
 	}]);
 	
